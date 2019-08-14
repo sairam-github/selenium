e77966139dd3c5fab201f0956d2b00a3f358015d
Sairam Madem <smadem@ptc.com>
2019-08-14 08:58:41 -0400

    Sleep time increased.

--- SeleniumDemo/src/test/java/PtcHomeTest.java
+++ SeleniumDemo/src/test/java/PtcHomeTest.java
@@ -26,7 +26,7 @@ public class PtcHomeTest {
 		contactUs.click();
 		
 		try {
-			Thread.sleep(6000);
+			Thread.sleep(7000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
